3b743bff55836a557d9ee85ff40653167e3bc7f4
sujantimalsina-np <80806689+sujantimalsina-np@users.noreply.github.com>
2021-05-14 16:06:15 +0545

    CSS Design: Glassmorphism
    Learning CSS tricks and new trends(Glassmorphism) for better UI design.

---
+++ Frontend Practise/view.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css">
+    <title>Glassmorphism</title>
+</head>
+
+<body>
+    <main>
+        <section class="glass">
+            <div class="dashboard">
+                <div class="user">
+                    <img src="images/user.png" alt="profile" height="50px" width="50px">
+                    <h3>Sujan Timalsina</h3>
+                    <p>Pro Member</p>
+                </div>
+                <div class="links">
+                    <div class="link">
+                        <img src="images/twitch.png">
+                        <h3>Streams</h3>
+                    </div>
+                    <div class="link">
+                        <img src="images/steam.png" alt="">
+                        <h3>Games</h3>
+                    </div>
+                    <div class="link">
+                        <img src="images/upcoming.png" alt="">
+                        <h3>New</h3>
+                    </div>
+                    <div class="link">
+                        <img src="images/library.png" alt="">
+                        <h3>Library</h3>
+                    </div>
+                </div>
+                <div class="pro">
+                    <h3>Join pro for free games.</h3>
+                    <img src="images/controller.png" alt="">
+                </div>
+            </div>
+            <div class="games">
+                <div class="status">
+                    <h1>Active Games</h1>
+                    <input type="text">
+                </div>
+                <div class="cards">
+                    <div class="card">
+                        <img src="images/assassins.png" alt="">
+                        <div class="card-info">
+                            <h3>Assassins Creed Valhalla</h3>
+                            <p>PS5 Version</p>
+                            <div class="progress"></div>
+                        </div>
+                        <h3 class="percentage">60%</h3>
+                    </div>
+                    <div class="card">
+                        <img src="images/assassins.png" alt="">
+                        <div class="card-info">
+                            <h3>Assassins Creed Valhalla</h3>
+                            <p>PS5 Version</p>
+                            <div class="progress"></div>
+                        </div>
+                        <h3 class="percentage">60%</h3>
+                    </div>
+                    <div class="card">
+                        <img src="images/assassins.png" alt="">
+                        <div class="card-info">
+                            <h3>Assassins Creed Valhalla</h3>
+                            <p>PS5 Version</p>
+                            <div class="progress"></div>
+                        </div>
+                        <h3 class="percentage">60%</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <div class="circle1"></div>
+    <div class="circle2"></div>
+</body>
+
+</html>
